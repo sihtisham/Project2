856b0607e8859fd02cbd2d4d5fff2a9f667e6b1e
moazabualrob <74381740+moazabualrob@users.noreply.github.com>
2020-11-18 13:37:23 -0500

    Update task1.ino
    Cleaned up unnecessary code and fixed formatting with explanations. Should be fine for Task 1. Needs testing on physical build

--- task1.ino
+++ task1.ino
@@ -4,8 +4,7 @@
 const int X_pin = 0; // analog pin connected to X output
 const int Y_pin = 1; // analog pin connected to Y output
 
-int val=0, val2=0, s, f, j, k;
-int servo=0, stepper=0;
+int val=0, val2=0, j;
 
 const int stepsPerRevolution = 256;
 
@@ -14,83 +13,59 @@ Stepper myStepper(stepsPerRevolution, 11, 9, 8, 10);
 
 
 void setup() {
-  myservo.attach(7);
-  myservo.write(90);
-  servo=90;
-  myStepper.setSpeed(60);
-
-  s=1;
-  f=1;
-  j=1;
-  k=1;
+    myservo.attach(7);
+    myservo.write(90);
+    myStepper.setSpeed(60);
   
-  Serial.begin(9600);
-}
+    j=80;
+
+    Serial.begin(9600);
+  }
 
 void loop() {
-val = analogRead(Y_pin);
-val2 = analogRead(X_pin);
-Serial.println(val2);
+    val = analogRead(Y_pin);
+    val2 = analogRead(X_pin);
+    Serial.println(val);
+    Serial.println(val2);
+    Serial.println();
+    
+   
+   //Stepper Motor Instructions 
+    if(val<500){
+        myStepper.step(j);
+        delay(50);
+        //When Joystick pushed up, move forward in j increments
+      }
+    
+    if(val>520){
+        myStepper.step(-j);
+        delay(50);
+        //When Joystick pushed down, move backwards in j increments
+      }
+    
+    if(val>501 && val<519){
+         delay(50);
+         //Dead Band -- When Joystick resting, do not move 
+      }
+        
+    //Servo Motor Instructions
+    if(val2>520){
+        myservo.write(180);
+        delay(100);
+        //When Joystick pushed to the right, move axle to the right
+      }
+    
+    if(val2<500){
+        myservo.write(0);
+        delay(100);
+        //When Joystick pushed to the left, move axle to the left
+      }
+    
+    if(val2>501 && val2<519){
+       myservo.write(90);
+       delay(100);
+       //Dead Band -- When Joystick resiting, return to neutral position
+      }
+    
 
-
-if(val<500){
-  j=j+1;
-  myStepper.step(j);
-  delay(50);
-}
-if(val<200){
-  j=j+50;
-  myStepper.step(j);
-  delay(50);
-}
-
-if(val>520){
-  k=k+1;
-  myStepper.step(-k);
-  delay(50);
   }
-if(val>900){
-  k=k+50;
-  myStepper.step(-k);
-  delay(50);
-  }
-
-if(val>501 && val<519){
-   j=1;
-   k=1;
-   delay(50);
-}
-
-if(val2>520){
-  s=s+1;
-   myservo.write(90+s);
-  delay(100);
-}
-if(val2>900){
-  s=s+20;
- // Serial.println(90+s);
-  delay(100);
-}
-
-if(val2<500){
-  f=f+1;
-   Serial.println(f);
-   myservo.write(90-f);
-  delay(100);
-}
-if(val2<200){
-  f=f+20;
- // Serial.println(90+s);
-  delay(100);
-}
-
-
-if(val2>501 && val2<519){
-   myservo.write(90);
-   s=1;
-   f=1;
-   delay(100);
-}
-
-
-}
